c6bbb29019d11daf4889b039593763e1000fb95a
Vladimir Zabolotskikh <m30b34735@gmail.com>
2023-09-21 07:27:06 +0300

    edit

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import pandas as pd
 
 
 def join_files(df1, df2, how, key):
-    different_fc_and_batt = df2.columns.difference(df1.columns).tolist()
-    different_fc_and_batt.append(key)
-    df2 = df2[different_fc_and_batt]
+    different_cols = df2.columns.difference(df1.columns).tolist()
+    different_cols.append(key)
+    df2 = df2[different_cols]
     return df1.merge(df2, how=how, on=key)
 
 
@@ -16,6 +16,7 @@ pilot = pd.read_csv('8/10_05_23__11_53__Pilot__139-FTS.csv')
 all_flight = join_files(batt, fc,how='outer', key=':Time')
 all_flight = join_files(all_flight, pilot, how='outer', key=':Time')
 all_flight = all_flight.fillna(0)
+
 print(all_flight.info())
 
-batt.to_csv('Flight_9.csv', index=False)
+all_flight.to_csv('Flight_8.csv', index=False)
